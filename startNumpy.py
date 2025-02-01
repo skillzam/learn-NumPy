e287f2503abc8da4be8e14cfc31ea9ff08340936
Skillzam <113600333+skillzam@users.noreply.github.com>
2025-02-01 14:54:13 +0530

    Create startNumpy.py

---
+++ startNumpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create a 2D array
+array = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Perform element-wise addition
+result = array + 10
+
+print("Original Array:\n", array)
+print("After Addition:\n", result)
